f6b7599ce0154cb73c2e4b05cf1fd957aaa35fe6
manhattan404 <76757456+manhattan404@users.noreply.github.com>
2022-03-16 21:10:43 -0400

    update

---
+++ final_main.py
@@ -0,0 +1,8 @@
+import json
+import random
+from colorama import Fore, Back, Style
+import os
+from menu import mainMenu
+from question_model import Question
+
+mainMenu()
--- menu.py
+++ menu.py
@@ -15,4 +15,3 @@ def mainMenu():
 
     print(myTable)
 
-mainMenu()
